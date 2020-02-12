8ca432c1a5df23c7c53724f80b8b797ad739e143
BobBobson87 <45653021+BobBobson87@users.noreply.github.com>
2020-02-12 15:45:04 +0100

    Add files via upload
    -Reformatted the python files to the PEP8 - standard
    -Added comments and any other explanations
    -Added real time data updating function (JQuery)
    -Fixed the duplicated entries issue
    -Added a world map, all stations where data is accumulated from can be seen on it
    -Rewrote some functions (they were thinking wayyy to hard lol)

--- Web Application/application.py
+++ Web Application/application.py
@@ -1,23 +1,25 @@
-from tempfile import mkdtemp
-
-from flask import Flask, redirect, render_template, request, session, flash, send_file
-from flask_session import Session
-
-from functions import login_required
-from lxml import etree as XML
-from functions import stnConverter
-from functions import nameAllocator
-from functions import sequenceCorrector
 import os
 import re
+import datetime
+import json
+from tempfile import mkdtemp
+from threading import Timer
+from flask import Flask, redirect, render_template, request, session, flash, send_file, jsonify
+from flask_session import Session
+from lxml import etree as XML
+from functions import login_required
+from functions import mapStationNumbers
+from functions import setStation_data
+
 
 # Configure application
 app = Flask(__name__)
 app.secret_key = 'super serial'
 
-# Reload templates when they are changed
 
+# Reload templates when they are changed
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
@@ -25,135 +27,54 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-#list of data for the table
-data = []
-data2 = [1,2]
-values_high = dict()
-values_low = dict()
-# with open("data.txt", 'r') as f:
-#     stations = stnConverter()
-#     #print(stations)
-#     #print(f)
-#     data = [line.replace('\n', '') for line in f]
-#     #print(data)
-#     data = [line.replace('\t', ',') for line in data]
-#     #print data
-#     data = [line.split(",") for line in data]
-#     print(data)
-#     data = nameAllocator(data, stations)
 
-path = "/etc/DataStore/Website/"
-
-stations = stnConverter()
-
-#THIS 'WITH OPEN' IS FOR READING FROM A DIRECTORY#######################################################
-#filelist = os.listdir(path)
-#for file in filelist:
-   # with open(os.path.join(path + file)) as f:
-
-#THIS 'WITH OPEN' IS FOR TESTDATA!!!!! #############################################################
-with open('2020-02-03_11_16_23.txt', 'r') as f:
-
-        for line in f:
-            #print(line)
-            line.replace("\n", "")
-            Values = line.split("_")
-            Values.append(Values[2])
-            Values[2] = Values[1]
-            Values[1] = Values[0]
-            #print(Values)
-            for (j, item) in enumerate(Values):
-
-                temp_sequence = item
-                temp_STN = Values[1]
-                print(data2)
-                #print(Values[1])
-                if temp_STN == '337450':
-                    #print("Moldova1")
-                    data2[0] = float(Values[2])
-                    data2[1]=float(Values[3].replace("\n", ""))
-                    break
-
-                elif temp_STN == '338150':
-                    #print("Moldova2")
-                    data2[0] = float(Values[2])
-                    data2[1] = float(Values[3].replace("\n", ""))
-                    break
-
-                elif temp_STN == '338830':
-                    #print("Moldova3")
-                    data2[0] = float(Values[2])
-                    data2[1] = float(Values[3].replace("\n", ""))
-                    
-                    break
-
-                for i in stations:
-                    station = i[0]
-                    name = i[1]
-                    #print(temp_sequence[0])
-                    #print(station)
-                if temp_STN == station:
-                    
-                    #print("found station", name)
-                    #print(item + name)
-                    item = name
-                else:
-                    pass
-            #print(sequence)
-            #return sequence
-
-            new_values = nameAllocator(Values,stations)
-            data.append(Values)
-data = nameAllocator(data, stations)
-
-print(data2)
-
-    
+# path = "/etc/DataStore/Website/"
+path = "files/"
 
 
+# Initialize the necessary variables
+station_data, locations = mapStationNumbers("static/stations_test.csv", "static/initialization.txt")
+setStation_data(station_data)
+moldova_dict = {}
+global_var = 0
 
+
+# Forward the user to the home page, only if the user has logged in.
 @app.route("/")
 @login_required
 def index():
-    return render_template("home.html")
+    return render_template("home.html", geolocations=locations)
 
 
+# Allow the user to login with the right credentials
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST" and request.form['password'] == 'password' and request.form['email'] == 'admin@admin':
         session['logged_in'] = True
+        # Read files in the web application directory
+        checkDirectoryAtCertainInterval()
         return redirect("/")
     elif request.method == "POST":
         flash('Wrong username or password!')
-
     return render_template("login.html")
 
 
-py_tempC = int(round(-100,2))
-py_tempDew = int(round(10, 2))
-print(py_tempC)
-print(py_tempDew)
-@app.route("/dashboard")
+# Forward the user to the dashboard, only if the user has logged in.
+@app.route("/dashboard", methods=["GET", "POST"])
 @login_required
 def dashboard():
-    return render_template("dashboard.html", len = len(data), data=data)
+    return render_template("dashboard.html", stations=station_data, date_EEU=datetime.datetime.now().strftime('%d-%m-%y'), date_moldova=datetime.datetime.now().strftime('%H:%M:%S'))
 
 
-
-# with open(dewpoint.txt, "r") as f:
-#     data_dewpoint = [line.replace('\n', '') for line in f]
-#     data_dewpoint = [line.replace('\t', '') for line in data_dewpoint]
-#     data_dewpoint = [line.split(',') for line in data_dewpoint]
-
-
+# Forward the user to an xml file page, only if the user has logged in.
 @app.route("/weatherdata")
 @login_required
 def return_file():
-
     GenerateXML("weatherdata.xml")
-
     return send_file('weatherdata.xml', attachment_filename="weatherdata.xml")
 
+
+# Logout function, allow the user to logout.
 @app.route("/logout")
 @login_required
 def logout():
@@ -161,54 +82,111 @@ def logout():
     return redirect("/")
 
 
-#XML converter
+# XML converter
 def GenerateXML(fileName):
     data = []
+    root = XML.Element("root")
 
-    with open("data.txt", 'r') as f:
-        data = [line.replace('\n', '') for line in f]
-        data = [line.replace('\t', ',') for line in data]
-        data = [line.split(",") for line in data]
+    for key, values in station_data.items():
+        weatherdata = XML.SubElement(root, "Weatherdata")
+        Id = XML.SubElement(weatherdata, "StationId")
+        Id.text = str(key)
 
+        country = XML.SubElement(weatherdata, "Country")
+        country.text = str(values[0])
 
-    root=XML.Element("root")
+        region = XML.SubElement(weatherdata, "Region")
+        region.text = str(values[1])
 
+        airpsi = XML.SubElement(weatherdata, "Air_Pressure")
+        airpsi.text = str(values[2])
 
+        time = XML.SubElement(weatherdata, "Time_Updated")
+        time.text = str(datetime.datetime.now().strftime('%d-%m-%y'))
 
-    for line in data:
-        weatherdata=XML.SubElement(root, "Weatherdata")
-        #print(line[3])
-        Id=XML.SubElement(weatherdata, "StationId")
-        Id.text = str(line[1])
+    tree = XML.ElementTree(root)
 
-        country=XML.SubElement(weatherdata, "Country")
-        country.text = str(line[0])
-
-        airpsi=XML.SubElement(weatherdata, "Air_Pressure")
-        airpsi.text = str(line[2])
-
-        time=XML.SubElement(weatherdata, "Time_Updated")
-        time.text = str(line[3])
-
-    tree=XML.ElementTree(root)
     with open(fileName, "wb") as files:
-        tree.write(files, encoding= 'utf8', pretty_print= True)
-
-if __name__ == "__main__":
-     GenerateXML("test.xml")
-    
+        tree.write(files, encoding='utf8', pretty_print=True)
 
 
-# if __name__ == '__main__':
-#     # Reload templates when they are changed
-#     app.config["TEMPLATES_AUTO_RELOAD"] = True
+# This function checks whether the specified directory contains any files that contain parsed data.
+def checkDirectoryAtCertainInterval():
+    # Make all global variables listed below usable in the scope of this function
+    global global_var
+    global moldova_dict
+    global station_data
+    global air_pressure_list
 
-#     # Configure session to use filesystem (instead of signed cookies)
-#     app.config["SESSION_FILE_DIR"] = mkdtemp()
-#     app.config["SESSION_PERMANENT"] = False
-#     app.config["SESSION_TYPE"] = "filesystem"
-#     Session(app)
+    # List all files in the current directory, specified by the path variable
+    filelist = os.listdir(path)
+    if len(filelist) > 0:
+        print("here")
+        moldova_data = {}
+        for file in filelist:
+            print("file: ", file)
+            filepath = os.path.join(path + file)
+            size = os.path.getsize(filepath)
+            with open(filepath) as f:
+                if size > 0:
+                    moldova_list = []
+                    if size > 1024:
+                        air_pressure_list = []
+                        for line in f:
+                            values = line.split("_")
+                            # Check whether the current station number of the current weather station is equal to any of the station numbers
+                            # of any of the station located in Moldova.
+                            if values[0] == '337450' or values[0] == '338150' or values[0] == '338830':
+                                # Convert the values and store them in python variables
+                                station = int(values[0])
+                                temp = values[1]
+                                dewp = values[2][:-1]
+                                # Create a fixed datastructure that can be easily used to convert python variables into javascript variables.
+                                moldova_data = {
+                                    'station_nr': station,
+                                    'temp': float(temp),
+                                    'dewp': float(dewp)
+                                }
+                                moldova_list.append(moldova_data)
+                            else:
+                                if ' \n' not in values and '\n' not in values:
+                                    air_pressure_list.append((values[0], values[1].replace('\n', '')))
 
-#     app.run()
+                        moldova_dict['moldova' + str(global_var % 1)] = moldova_list
+
+                        for values in air_pressure_list:
+                            station_nr = int(values[0])
+                            if(station_data.get(station_nr) is not None):
+                                country = station_data.get(station_nr)[0]
+                                region = station_data.get(station_nr)[1]
+                                station_data.get(station_nr).clear()
+                                station_data.get(station_nr).append(country)
+                                station_data.get(station_nr).append(region)
+                                station_data.get(station_nr).append(float(values[1]))
+                                station_data.get(station_nr).append(datetime.datetime.now().strftime('%d-%m-%y'))
+                    else:
+                        for line in f:
+                            values = line.split("_")
+                            if ' \n' not in values and '\n' not in values:
+                                station = int(values[0])
+                                data = {
+                                    'station_nr': station,
+                                    'temp': float(values[1]),
+                                    'dewp': float(values[2][:-2])
+                                }
+                                moldova_list.append(data)
+                        moldova_dict['moldova' + str(global_var % 1)] = moldova_list
+            global_var += 1
+            os.remove(f.name)
+            print(moldova_dict)
+    if moldova_dict == {} and len(os.listdir(path)) == 0:
+        moldova_dict['moldova' + str(0)] = [{'station_nr': 338150, 'temp': 0, 'dewp': 0},
+                                            {'station_nr': 338830, 'temp': 0, 'dewp': 0}, {'station_nr': 337450, 'temp': 0, 'dewp': 0}]
+    Timer(1.0, checkDirectoryAtCertainInterval).start()
 
 
+# Create a route, this route can be accessed by the JQuery command used in dashboard.html
+@app.route("/dashboard_data")
+def returnData():
+    global moldova_dict
+    return jsonify(moldova_dict)
